75cbb60a869866906be08b47aff79e94b411c33a
Krishna Singh <144099384+krishnasingh34@users.noreply.github.com>
2025-03-01 16:19:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//your JS code here. If required.
 const output = document.getElementById("output");
 const btn = document.getElementById("download-images-button");
 const errorDiv = document.getElementById("error");
@@ -9,43 +8,38 @@ const images = [
   { url: "https://picsum.photos/id/238/200/300" },
   { url: "https://picsum.photos/id/239/200/300" },
 ];
-btn.addEventListener("click", downloadImages)
-function downloadImage(link){
-        return new Promise((resolve, reject)=>{
-            if(link.startsWith('https')){
-                setTimeout(()=>{
-                    resolve({
-                        image:link,
-                        message: 'Image downloaded successfully'
-                    })
-                },3000)
-            }
-            else{
-                setTimeout(()=>{
-                    reject(`Image could not be downloaded ${link}`)
-                },3000)
-            }
-        })
-       }
+
+function downloadImage(link) {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.src = link;
+
+    img.onload = () => resolve({ image: link, message: "Image loaded successfully" });
+    img.onerror = () => reject(`Failed to load image: ${link}`);
+  });
+}
+
 function downloadImages() {
   loading.style.display = "block";
   output.innerHTML = "";
   errorDiv.innerHTML = "";
-       let arr=[]
-       for(let img of images){
-        arr.push(downloadImage(img.url))
-       }
-       Promise.all(arr)
-       .then(res =>{
-        for(let t of res){
-            let img = document.createElement('img')
-            img.src = t.image
-            output.append(img)
-        }
-       })
-       .catch(err =>{
-        errorDiv.innerText = err;
-       })
-	  .finally(() => {
-      loading.style.display = "none"
+
+  const downloadPromises = images.map((img) => downloadImage(img.url));
+
+  Promise.all(downloadPromises)
+    .then((res) => {
+      res.forEach((t) => {
+        const img = document.createElement("img");
+        img.src = t.image;
+        output.appendChild(img);
+      });
+    })
+    .catch((err) => {
+      errorDiv.innerText = err;
+    })
+    .finally(() => {
+      loading.style.display = "none";
     });
+}
+
+btn.addEventListener("click", downloadImages);
